ee55c4232a99a8dc45fc137a82d009f2df1934da
Argha <argha.ray82@gmail.com>
2017-05-16 17:17:11 -0500

    Commit

--- app/src/main/java/com/example/speechtotext/DisplayWebView.java
+++ app/src/main/java/com/example/speechtotext/DisplayWebView.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -20,6 +22,8 @@ public class DisplayWebView extends AppCompatActivity {
         setContentView(R.layout.activity_display_webview);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setDrawingCacheEnabled(true);
+        myWebView.buildDrawingCache(true);
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.getSettings().setLoadWithOverviewMode(true);
         //WebSettings webSettings = myWebView.getSettings();
@@ -47,7 +51,7 @@ public class DisplayWebView extends AppCompatActivity {
         String summary = "There are two solutions to \\(ax^2 + bx + c = 0\\) and they are\n" +
                 "$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$\n";
 
-        if (android.os.Build.VERSION.SDK_INT < 19)
+        /*if (android.os.Build.VERSION.SDK_INT < 19)
         {
             myWebView.loadDataWithBaseURL("http://bar","<script type='text/javascript' "
                             +"src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_CHTML'>"
@@ -55,28 +59,41 @@ public class DisplayWebView extends AppCompatActivity {
                     "text/html","utf-8","");
         }
         else
-        {
+        {*/
             myWebView.loadDataWithBaseURL("http://bar","<script type='text/javascript' "
                             +"src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_CHTML'>"
                             +"</script><math display='block'>"+summary+"</math>",
                     "text/html","utf-8","");
-        }
+        //}
 
+
+
+
+    }
+
+    public void saveImage (View view) {
+
+        Log.d("Speech To Text","In saveImage");
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setDrawingCacheEnabled(true);
+        myWebView.buildDrawingCache(true);
         //Bitmap bitmap = Bitmap.createBitmap(myWebView.getWidth(), myWebView.getHeight(), Bitmap.Config.ARGB_8888);
-        Bitmap bitmap = Bitmap.createBitmap(50, 50, Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = Bitmap.createBitmap(myWebView.getDrawingCache());
         Canvas canvas = new Canvas(bitmap);
         myWebView.draw(canvas);
-        FileOutputStream fos = null;
+
         try {
-            fos = new FileOutputStream( "/sdcard/"  + "page.jpg" );
-            if ( fos != null ) {
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fos );
-                fos.close();
-            }
+            FileOutputStream fos = new FileOutputStream( "/sdcard/"  + "page.jpg" );
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fos );
+            fos.close();
+
         }
         catch( Exception e ) {
             System.out.println("-----error--"+e);
         }
+        myWebView.setDrawingCacheEnabled(false);
     }
+
 }
 
